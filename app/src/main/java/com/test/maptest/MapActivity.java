89ba37e1cac28d6cdf9f809c77e702f7c76b7c62
bguedon <guedon.bruno@gmail.com>
2017-10-24 17:39:50 +0200

    Added search address bar updating current location uses mabbox geocoder widget

--- app/src/main/java/com/test/maptest/MapActivity.java
+++ app/src/main/java/com/test/maptest/MapActivity.java
@@ -1,6 +1,5 @@
 package com.test.maptest;
 
-import android.*;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -10,17 +9,25 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Layout;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.Marker;
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.services.android.ui.geocoder.GeocoderAutoCompleteView;
+import com.mapbox.services.api.geocoding.v5.GeocodingCriteria;
+import com.mapbox.services.api.geocoding.v5.models.CarmenFeature;
+import com.mapbox.services.commons.models.Position;
 
 /**
  * Created by bguedon on 24/10/2017.
@@ -31,9 +38,11 @@ public class MapActivity extends AppCompatActivity implements GoogleApiClient.Co
     private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 0;
     private MapView mMapView;
     private MapboxMap mMapboxMap;
+    private Marker mCurrentLocationMarker;
+
 
     private GoogleApiClient mGoogleApiClient;
-    private Location mLastLocation;
+    private Location mCurrentLocation;
 
 
     @Override
@@ -61,16 +70,57 @@ public class MapActivity extends AppCompatActivity implements GoogleApiClient.Co
 
             }
         });
+
+        // Address auto complete view
+        GeocoderAutoCompleteView autocomplete = (GeocoderAutoCompleteView) findViewById(R.id.query);
+        autocomplete.setAccessToken(Mapbox.getAccessToken());
+        autocomplete.setType(GeocodingCriteria.TYPE_ADDRESS);
+        autocomplete.setOnFeatureListener(new GeocoderAutoCompleteView.OnFeatureListener() {
+            @Override
+            public void onFeatureClick(CarmenFeature feature) {
+                hideKeyboard();
+                Position position = feature.asPosition();
+                updateCurrentLocation(position.getLatitude(), position.getLongitude());
+                updateCurrentLocationMarker();
+            }
+        });
+
     }
 
     @SuppressWarnings( {"MissingPermission"})
-    private void updateLastLocation() {
-        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+    private void updateLastknownLocation() {
+        mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(
                 mGoogleApiClient);
-        if (mLastLocation != null) {
-            mMapboxMap.addMarker(new MarkerOptions().setPosition(new LatLng(mLastLocation)));
-            mMapboxMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mLastLocation), 16));
+    }
+
+    private void updateCurrentLocation(double latitude, double longitude) {
+        mCurrentLocation.reset();
+        mCurrentLocation.setLatitude(latitude);
+        mCurrentLocation.setLongitude(longitude);
+    }
+
+    private void updateCurrentLocationMarker() {
+        if (mCurrentLocation != null) {
+            if (mCurrentLocationMarker == null) {
+                mCurrentLocationMarker = mMapboxMap.addMarker(new MarkerOptions().setPosition(new LatLng(mCurrentLocation)));
+            } else {
+                mCurrentLocationMarker.setPosition(new LatLng(mCurrentLocation));
+                mMapboxMap.updateMarker(mCurrentLocationMarker);
+            }
+            mMapboxMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mCurrentLocation), 16));
         }
+    }
+
+    private void hideKeyboard() {
+        try {
+            InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            if (getCurrentFocus() != null) {
+                imm.hideSoftInputFromWindow(this.getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+            }
+        } catch (Exception exception) {
+            throw new RuntimeException(exception);
+        }
+
     }
 
     @Override
@@ -80,8 +130,8 @@ public class MapActivity extends AppCompatActivity implements GoogleApiClient.Co
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    updateLastLocation();
+                    updateLastknownLocation();
+                    updateCurrentLocationMarker();
 
                 } else {
 
@@ -136,11 +186,12 @@ public class MapActivity extends AppCompatActivity implements GoogleApiClient.Co
         mMapView.onSaveInstanceState(outState);
     }
 
-    // google api client callbacks
+      // google api client callbacks
     @Override
     public void onConnected(@Nullable Bundle bundle) {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            updateLastLocation();
+            updateLastknownLocation();
+            updateCurrentLocationMarker();
         } else {
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
