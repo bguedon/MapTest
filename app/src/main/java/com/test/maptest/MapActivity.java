8f029a54466f8dc0904d8248b85333a1836efd32
bguedon <guedon.bruno@gmail.com>
2017-10-25 10:40:15 +0200

    Added reverse geocoding call on camera idle Display address formatted with number street postcode city Fixed autocomplete display to expand up to 3 lines

--- app/src/main/java/com/test/maptest/MapActivity.java
+++ app/src/main/java/com/test/maptest/MapActivity.java
@@ -10,6 +10,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Layout;
+import android.util.Log;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
 
@@ -19,6 +20,7 @@ import com.google.android.gms.location.LocationServices;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.annotations.Marker;
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
@@ -26,8 +28,16 @@ import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.services.android.ui.geocoder.GeocoderAutoCompleteView;
 import com.mapbox.services.api.geocoding.v5.GeocodingCriteria;
+import com.mapbox.services.api.geocoding.v5.MapboxGeocoding;
 import com.mapbox.services.api.geocoding.v5.models.CarmenFeature;
+import com.mapbox.services.api.geocoding.v5.models.GeocodingResponse;
 import com.mapbox.services.commons.models.Position;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by bguedon on 24/10/2017.
@@ -35,10 +45,15 @@ import com.mapbox.services.commons.models.Position;
 
 public class MapActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
+    private static final String TAG = "com.test.MapTest";
+
     private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 0;
     private MapView mMapView;
     private MapboxMap mMapboxMap;
     private Marker mCurrentLocationMarker;
+
+    GeocoderAutoCompleteView autocomplete;
+    MapboxMap.OnCameraIdleListener onCameraIdleListener;
 
 
     private GoogleApiClient mGoogleApiClient;
@@ -62,20 +77,54 @@ public class MapActivity extends AppCompatActivity implements GoogleApiClient.Co
 
         mMapView = (MapView) findViewById(R.id.mapView);
         mMapView.onCreate(savedInstanceState);
+        onCameraIdleListener = new MapboxMap.OnCameraIdleListener() {
+            @Override
+            public void onCameraIdle() {
+                MapboxGeocoding reverseGeocode = new MapboxGeocoding.Builder()
+                        .setAccessToken(Mapbox.getAccessToken())
+                        .setCoordinates(Position.fromCoordinates(mMapboxMap.getCameraPosition().target.getLongitude(),mMapboxMap.getCameraPosition().target.getLatitude()))
+                        .setGeocodingType(GeocodingCriteria.TYPE_ADDRESS)
+                        .build();
+                reverseGeocode.enqueueCall(new Callback<GeocodingResponse>() {
+                    @Override
+                    public void onResponse(Call<GeocodingResponse> call, Response<GeocodingResponse> response) {
+                        List<CarmenFeature> results = response.body().getFeatures();
+                        if (results.size() > 0) {
+                            // TODO add parse method and check context size
+                            String address = (results.get(0).getAddress() != null ? results.get(0).getAddress() + " " :  "") + results.get(0).getText() + " " + results.get(0).getContext().get(0).getText() + " " + results.get(0).getContext().get(1).getText();
+                            Log.d(TAG, "onResponse: " + address);
+                            autocomplete.setText(address);
+                            autocomplete.setSelection(address.length());
+                            autocomplete.cancelApiCall();
+                            autocomplete.dismissDropDown();
+                            hideKeyboard();
+                        } else {
+                            // No result for your request were found.
+                            Log.d(TAG, "onResponse: No result found");
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<GeocodingResponse> call, Throwable throwable) {
+                        throwable.printStackTrace();
+                    }
+                });
+            }
+        };
         mMapView.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(MapboxMap mapboxMap) {
                 mMapboxMap = mapboxMap;
                 mGoogleApiClient.connect();
-
-
             }
         });
 
         // Address auto complete view
-        GeocoderAutoCompleteView autocomplete = (GeocoderAutoCompleteView) findViewById(R.id.query);
+        autocomplete = (GeocoderAutoCompleteView) findViewById(R.id.query);
         autocomplete.setAccessToken(Mapbox.getAccessToken());
         autocomplete.setType(GeocodingCriteria.TYPE_ADDRESS);
+        autocomplete.setSingleLine(false);
+        autocomplete.setMaxLines(3);
         autocomplete.setOnFeatureListener(new GeocoderAutoCompleteView.OnFeatureListener() {
             @Override
             public void onFeatureClick(CarmenFeature feature) {
@@ -107,7 +156,10 @@ public class MapActivity extends AppCompatActivity implements GoogleApiClient.Co
             } else {
                 mCurrentLocationMarker.setPosition(new LatLng(mCurrentLocation));
             }
-            mMapboxMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mCurrentLocation), 16));
+            mMapboxMap.removeOnCameraIdleListener(onCameraIdleListener);
+            mMapboxMap.setLatLng(new LatLng(mCurrentLocation));
+            mMapboxMap.setZoom(16);
+            mMapboxMap.addOnCameraIdleListener(onCameraIdleListener);
         }
     }
 
